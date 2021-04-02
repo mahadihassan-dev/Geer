2620aac97a9c991005e14866a29f94765a6b4020
mahedihassan-dev <mahadihassan.mhd@gmail.com>
2021-04-02 21:53:23 +0600

    Geer Full Update Version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+     // Slider Section
+     $('.banner_slider').slick({
+         autoplay: true,
+         speed: 2000,
+         arrows: false,
+         dots: false,
+         fade: true,
+     });
+ });
